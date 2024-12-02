17c090a6988c019eb767b3cf1fa7fbcabd30f0a8
PARKPARKWOO <wy9295@naver.com>
2024-12-02 20:28:12 +0900

    feat: Add route rules and docker file

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,18 +17,30 @@ java {
 repositories {
     maven { url = uri("https://repo.spring.io/milestone") }
     mavenCentral()
+    maven {
+        name = "GitHubPackages"
+        url = uri("https://maven.pkg.github.com/PARKPARKWOO/common-module")
+        credentials {
+            username = project.findProperty("gpr.user")?.toString() ?: System.getenv("GITHUB_USERNAME")
+            password = project.findProperty("gpr.key")?.toString() ?: System.getenv("GITHUB_TOKEN")
+        }
+    }
 }
 
 extra["springCloudVersion"] = "2024.0.0-RC1"
 
 dependencies {
+    implementation("org.woo:domain-auth:0.0.5-SNAPSHOT")
+    implementation("org.woo:http:+")
+    implementation("org.woo:mapper:+")
+    implementation("org.woo:log:0.0.2-SNAPSHOT")
+
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j")
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
-    implementation("org.springframework.cloud:spring-cloud-starter-gateway-mvc")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
