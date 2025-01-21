4acdf726063e2663118289396ef5e4811f83bba9
PARKPARKWOO <wy9295@naver.com>
2025-01-21 15:09:08 +0900

    feat: Update AuthenticateGrpcFilter

--- src/main/kotlin/org/woo/gateway/filter/AuthenticateGrpcFilter.kt
+++ src/main/kotlin/org/woo/gateway/filter/AuthenticateGrpcFilter.kt
@@ -7,8 +7,11 @@ import kotlinx.coroutines.runBlocking
 import model.Role
 import org.springframework.cloud.gateway.filter.GatewayFilter
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory
+import org.springframework.http.HttpHeaders
 import org.springframework.http.server.reactive.ServerHttpRequest
+import org.springframework.http.server.reactive.ServerHttpRequestDecorator
 import org.springframework.stereotype.Component
+import org.springframework.web.server.ServerWebExchange
 import org.woo.apm.log.log
 import org.woo.auth.grpc.AuthProto.UserInfoResponse
 import org.woo.gateway.client.GrpcAuthClient
@@ -26,26 +29,28 @@ class AuthenticateGrpcFilter(
         val bearerToken = request.headers["Authorization"]?.firstOrNull()
         val hasToken = bearerToken != null && bearerToken.startsWith("Bearer ")
         return@GatewayFilter if (hasToken) {
-            runBlocking {
+            val modifiedExchange: ServerWebExchange = runBlocking {
                 runCatching {
-                    request.requestAuthAndSetPassport(bearerToken!!)
+                    exchange.requestAuthAndSetPassport(bearerToken!!)
                 }.onFailure { exception ->
                     log().warn("Authentication failed ${exception.message}")
+                }.getOrElse {
+                    exchange
                 }
             }
-            chain.filter(exchange)
+            chain.filter(modifiedExchange)
         } else {
             chain.filter(exchange)
         }
     }
 
-    suspend fun ServerHttpRequest.requestAuthAndSetPassport(bearerToken: String) = mono {
+    suspend fun ServerWebExchange.requestAuthAndSetPassport(bearerToken: String):ServerWebExchange = mono {
         val passport = authClient.getUserInfo(bearerToken)
         log().info("authenticate userId = ${passport.id}")
         setPassport(passport)
     }.awaitSingle()
 
-    fun ServerHttpRequest.setPassport(passport: UserInfoResponse) {
+    fun ServerWebExchange.setPassport(passport: UserInfoResponse): ServerWebExchange {
         val userContext = UserContext(
             userId = UUID.fromString(passport.id),
             userName = passport.name,
@@ -53,6 +58,13 @@ class AuthenticateGrpcFilter(
             email = passport.email,
         )
         val userContextString = Jackson.writeValueAsString(userContext)
-        this.headers.add("X-User-Passport", userContextString)
+        val headers = HttpHeaders()
+        headers.putAll(this.request.headers)
+        headers.add("X-User-Passport", userContextString)
+
+        val modifiedRequest = object : ServerHttpRequestDecorator(this.request) {
+            override fun getHeaders(): HttpHeaders = headers
+        }
+        return this.mutate().request(modifiedRequest).build()
     }
 }
