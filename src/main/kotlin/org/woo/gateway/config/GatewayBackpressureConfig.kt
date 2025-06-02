629a879aabaad3b467fc5f12ce293a4cfc8f2888
PARKPARKWOO <wy9295@naver.com>
2025-06-02 23:04:25 +0900

    feat: Add validation logic for cookie

--- src/main/kotlin/org/woo/gateway/config/GatewayBackpressureConfig.kt
+++ src/main/kotlin/org/woo/gateway/config/GatewayBackpressureConfig.kt
@@ -14,30 +14,31 @@ import reactor.netty.resources.ConnectionProvider
 
 @Configuration
 class GatewayBackpressureConfig {
-    @Bean
-    fun httpClient(resourceFactory: ReactorResourceFactory): HttpClient {
-        val provider = resourceFactory.connectionProvider
-        // ConnectionProvider 설정 예시 (최대 커넥션, 대기 큐 등)
-            ?: ConnectionProvider.builder("limited-provider")
-                .maxConnections(50)
-                .pendingAcquireMaxCount(500)
-                .build()
-        return HttpClient.create(provider)
-            .option(
-                ChannelOption.WRITE_BUFFER_WATER_MARK,
-                WriteBufferWaterMark(
-                    32 * 1024,
-                    64 * 1024
-                )
-            ).doOnConnected{ conn ->
-                conn.addHandlerLast(ReadTimeoutHandler(1))
-                conn.addHandlerLast(WriteTimeoutHandler(1))
-            }.http2Settings { http2Settings ->
-
-            }
-    }
-
-    @Bean
-    fun clientHttpConnector(httpClient: HttpClient): ClientHttpConnector =
-        ReactorClientHttpConnector(httpClient)
+    //TODO: Backpressure 설정을 한다.
+//    @Bean
+//    fun httpClient(resourceFactory: ReactorResourceFactory): HttpClient {
+//        val provider = resourceFactory.connectionProvider
+//        // ConnectionProvider 설정 예시 (최대 커넥션, 대기 큐 등)
+//            ?: ConnectionProvider.builder("limited-provider")
+//                .maxConnections(50)
+//                .pendingAcquireMaxCount(500)
+//                .build()
+//        return HttpClient.create(provider)
+//            .option(
+//                ChannelOption.WRITE_BUFFER_WATER_MARK,
+//                WriteBufferWaterMark(
+//                    32 * 1024,
+//                    64 * 1024
+//                )
+//            ).doOnConnected{ conn ->
+//                conn.addHandlerLast(ReadTimeoutHandler(1))
+//                conn.addHandlerLast(WriteTimeoutHandler(1))
+//            }.http2Settings { http2Settings ->
+//
+//            }
+//    }
+//
+//    @Bean
+//    fun clientHttpConnector(httpClient: HttpClient): ClientHttpConnector =
+//        ReactorClientHttpConnector(httpClient)
 }
